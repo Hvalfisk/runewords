5f6087ee1bab52f0d126d3e91741138180b9b075
Jakob Kofod Beyer <jkb@hvalfisk.dk>
2022-06-19 23:42:11 +0200

    Persist filter on reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { runes, runewords } from './runewords';
 import { ItemCategory, Rune, Runeword } from './types/runeword';
-import { BehaviorSubject, combineLatest, filter, map, Observable, shareReplay, startWith, tap } from 'rxjs';
+import { BehaviorSubject, combineLatest, filter, map, Observable, shareReplay, startWith, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'runewords';
   readonly runewords: ReadonlyArray<Runeword> = runewords;
   readonly runes: ReadonlyArray<Rune> = runes;
   readonly sockets = [2, 3, 4, 5, 6];
   readonly itemCategories: ItemCategory[] = ['armor', 'head', 'melee', 'ranged', 'shield'];
-  readonly searchControl = new FormControl('');
+  readonly searchControl = new FormControl(sessionStorage.getItem('search') ?? '');
   readonly runesCheckedSubject = new BehaviorSubject<Record<Rune, boolean>>(
     this.runes.reduce(
       (acc, curr) => ({ ...acc, [curr]: false }), {} as Partial<Record<Rune, boolean>>
-    ) as Required<Record<Rune, boolean>>
+    ) as Record<Rune, boolean>
   );
   readonly runes$: Observable<({ value: Rune, checked: boolean })[]> = this.runesCheckedSubject.pipe(
     map(runesChecked =>
@@ -41,9 +41,10 @@ export class AppComponent {
       )
     );
 
+  readonly searchValue$ = this.searchControl.valueChanges.pipe(startWith(this.searchControl.value), filter((s: any): s is string => typeof s === 'string'), shareReplay(1));
 
   readonly filter$ = combineLatest([
-    this.searchControl.valueChanges.pipe(startWith(this.searchControl.value), filter((s: any): s is string => typeof s === 'string'), shareReplay(1)),
+    this.searchValue$,
     this.runes$.pipe(map(v => this.extractChecked(v))),
     this.sockets$.pipe(map(v => this.extractChecked(v))),
     this.itemCategories$.pipe(map(v => this.extractChecked(v)))
@@ -78,18 +79,44 @@ export class AppComponent {
   constructor() {
   }
 
+  private readonly destroyed = new Subject<void>();
+
+  ngOnInit(): void {
+    const runesString = sessionStorage.getItem('runes');
+    if (runesString !== null && runesString.length > 0) {
+      this.runesCheckedSubject.next(JSON.parse(runesString));
+    }
+    const socketsString = sessionStorage.getItem('sockets');
+    if (socketsString !== null && socketsString.length > 0) {
+      this.socketsCheckedSubject.next(JSON.parse(socketsString));
+    }
+    const itemCategoriesString = sessionStorage.getItem('itemCategories');
+    if (itemCategoriesString !== null && itemCategoriesString.length > 0) {
+      this.itemCategoriesCheckedSubject.next(JSON.parse(itemCategoriesString));
+    }
+    this.searchControl.valueChanges.pipe(filter((v): v is string => typeof v === 'string'), takeUntil(this.destroyed)).subscribe(v => sessionStorage.setItem('search', v));
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
+
   toggleRune(rune: Rune, eventTarget: EventTarget | null) {
     const checked = eventTarget instanceof HTMLInputElement ? eventTarget.checked : false;
     this.runesCheckedSubject.next({ ...this.runesCheckedSubject.value, [rune]: checked })
+    sessionStorage.setItem('runes', JSON.stringify(this.runesCheckedSubject.value));
   }
 
   toggleSocket(socket: number, eventTarget: EventTarget | null) {
     const checked = eventTarget instanceof HTMLInputElement ? eventTarget.checked : false;
     this.socketsCheckedSubject.next({ ...this.socketsCheckedSubject.value, [socket]: checked })
+    sessionStorage.setItem('sockets', JSON.stringify(this.socketsCheckedSubject.value));
   }
 
   toggleItemCategory(itemCategory: ItemCategory, eventTarget: EventTarget | null) {
     const checked = eventTarget instanceof HTMLInputElement ? eventTarget.checked : false;
     this.itemCategoriesCheckedSubject.next({ ...this.itemCategoriesCheckedSubject.value, [itemCategory]: checked })
+    sessionStorage.setItem('itemCategories', JSON.stringify(this.itemCategoriesCheckedSubject.value));
   }
 }
